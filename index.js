2db576f94a0832512af19e85194d87d0326e63fd
katherinekhine <khaingkatherine@gmail.com>
2024-10-02 14:03:35 +0630

    update index.js and schema.js

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ const resolvers = {
       return db.reviews.find((review) => review.id === args.id);
     },
   },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.author_id === parent.id);
+    },
+  },
+  Review: {
+    game(parent) {
+      return db.games.find((g) => g.id === parent.game_id);
+    },
+    author(parent) {
+      return db.authors.find((a) => a.id === parent.author_id);
+    },
+  },
 };
 
 // Server Setup inside an async function
--- schema.js
+++ schema.js
@@ -2,19 +2,23 @@ export const typeDefs = `#graphql
 type Game {
     id: ID!  # ID is non-nullable
     title: String!  
-    platform: [String!]!  
+    platform: [String!]! 
+    reviews: [Review!]!  
 }
 
 type Review {
     id: ID!
     rating: Int!
     content: String!
+    game: Game!
+    author: Author!
 }
 
 type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review!]!
 }
 
 type Query {
